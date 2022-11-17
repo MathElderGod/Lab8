47c274e7c19c0b889cade3f9fc612a71c9216d15
Alexander G. Arias <agarias@ucsd.edu>
2022-11-17 12:10:48 -0800

    minor fixes

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -6,37 +6,37 @@ const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // isPhoneNumber 4 tests
 // isPhoneNumber first truth test
-test('checks to see if a number is in ###-###-#### valid form', () => {
+test('checks to see if the number 760-996-4461 is valid', () => {
     expect(functions.isPhoneNumber('760-996-4461')).toBe(true);
 });
 // isPhoneNumber second truth test
-test('checks to see if a number is in (###) ###-#### or ######-#### valid form', () => {
+test('checks to see if the number (760) 996-4461 is valid', () => {
     expect(functions.isPhoneNumber('(760) 996-4461')).toBe(true);
 });
 // isPhoneNumber first false test
-test('checks to see if a number is not in the specified valid forms', () => {
+test('checks to see if the number 7609964461 is invalid', () => {
     expect(functions.isPhoneNumber('7609964461')).toBe(false);
 });
 // isPhoneNumber second false test
-test('checks to see if an empty phone number is valid', () => {
+test('checks to see if an empty phone number is invalid', () => {
     expect(functions.isPhoneNumber('')).toBe(false);
 });
 
 // isEmail 4 unit tests
 // isEmail first truth test
-test('checks to see if an email is in name@url.com valid form', () => {
+test('checks to see if the email agarias@ucsd.edu is valid', () => {
     expect(functions.isEmail('agarias@ucsd.edu')).toBe(true);
 });
 // isEmail second truth test
-test('checks to see if an email is in name # @url.co valid form', () => {
+test('checks to see if the email agarias2462@ucsd.ed is valid', () => {
     expect(functions.isEmail('agarias2462@ucsd.ed')).toBe(true);
 });
 // isEmail first false test
-test('checks to see if an email is in name.name@url.c invalid form', () => {
+test('checks to see the email agarias2462isthebest@ucsd.e is invalid', () => {
     expect(functions.isEmail('agarias2462isthebest@ucsd.e')).toBe(false);
 });
 // isEmail second false test
-test('checks to see if an email is in name.nameurl.com invalid form', () => {
+test('checks to see if the string agarias.ucsd.edu is an invalid email', () => {
     expect(functions.isEmail('agarias.ucsd.edu')).toBe(false);
 });
 
@@ -47,19 +47,19 @@ test('checks to see if an email is in name.nameurl.com invalid form', () => {
 
 // isStrongPassword 4 unit tests
 // isStrongPassword first truth test
-test('checks to see if a password is within 3 to 15', () => {
+test('checks to see if Ihatecse is a valid strong password', () => {
     expect(functions.isStrongPassword('Ihatecse')).toBe(true);
 });
 // isStrongPassword scond truth test
-test('checks to see if a password is meets the constraints', () => {
+test('checks to see if La_9278tak is a valid strong password', () => {
     expect(functions.isStrongPassword('La_9278take')).toBe(true);
 });
 // isStrongPassword first false test
-test('checks to see if a passwords first character is a letter', () => {
+test('checks to see if 9Ilivetolive is an invalid password', () => {
     expect(functions.isStrongPassword('9Ilivetolive')).toBe(false);
 });
 // isStrongPassword second false test
-test('checks to see if a password exceeds 15 characters', () => {
+test('checks to see if ahskdjflakjdakdhfahjfhjdskh is an invalid password', () => {
     expect(functions.isStrongPassword('ahskdjflakjdakdhfahjfhjdskh')).toBe(false);
 });
 
@@ -68,37 +68,37 @@ test('checks to see if a password exceeds 15 characters', () => {
 
 // isDate 4 unit tests
 // isDate first truth test
-test('checks to see if a date matches matches dates of the form XX / XX / YYYY exactly', () => {
+test('checks to see if 03/15/1995 is a valid date', () => {
     expect(functions.isDate('03/15/1995')).toBe(true);
 });
 // isDate second truth test
-test('checks to see if a date matches matches dates of the form X / X / YYYY exactly', () => {
+test('checks to see if 3/5/1995 is a valid date', () => {
     expect(functions.isDate('3/5/1995')).toBe(true);
 });
 // isDate first false test
-test('checks to see if a date matches violates the form X / X / YYYY with a two digit year', () => {
+test('checks to see if 3/5/95 is an invalid date', () => {
     expect(functions.isDate('3/5/95')).toBe(false);
 });
 // isDate second false test
-test('checks to see if an empty date is invalid', () => {
+test('checks to see if an empty date is an invalid date', () => {
     expect(functions.isDate('')).toBe(false);
 });
 
 // Matches valid 3 or 6 character hex codes used for HTML or CSS.
 // isHexColor 4 unit tests
 // isHexColor first truth test
-test('checks to see if hex code of three is valid', () => {
+test('checks to see if hex code of three #fc9 is valid', () => {
     expect(functions.isHexColor('#fc9')).toBe(true);
 });
 // isHexColor second truth test
-test('checks to see if hex code of six is valid', () => {
+test('checks to see if hex code of six #ffcc99 is valid', () => {
     expect(functions.isHexColor('#ffcc99')).toBe(true);
 });
 // isHexColor first false test
-test('checks to see if hex code of two is invalid', () => {
+test('checks to see if hex code of two #ff is invalid', () => {
     expect(functions.isHexColor('#ff')).toBe(false);
 });
 // isHexColor second false test
-test('checks to see if hex code of 7 is invalid', () => {
+test('checks to see if hex code of 7 #ffcc991 is invalid', () => {
     expect(functions.isHexColor('#ffcc991')).toBe(false);
 });
